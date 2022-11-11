27bfd3696c994dd31243944285870fe2e55d19be
Mohamad Ridzuan <mohamadridzuan@hotmail.com>
2022-11-11 09:09:09 +0800

    sysdiagnose-mobilecontainermanager.py

--- sysdiagnose-mobilecontainermanager.py
+++ sysdiagnose-mobilecontainermanager.py
@@ -4,10 +4,14 @@
 # Script to print uninstall infor from logs/MobileContainerManager/containermanagerd.log.0
 # Author: cheeky4n6monkey@gmail.com
 
-import sys
+import sys, re
 from optparse import OptionParser
 
-version_string = "sysdiagnose-mobilecontainermanager.py v2019-05-10 Version 1.0"
+def find_app(input_text):
+    app = re.search(r"[A-Za-z0-9]+\.[A-Za-z0-9]+\.[A-Za-z0-9]+", input_text)
+    return app.group()
+
+version_string = "sysdiagnose-mobilecontainermanager.py v2022-11-11 Version 1.1"
 
 if sys.version_info[0] < 3:
     print("Must be using Python 3! Exiting ...")
@@ -18,9 +22,9 @@ print("Running " + version_string + "\n")
 usage = "\n%prog -i inputfile\n"
 
 parser = OptionParser(usage=usage)
-parser.add_option("-i", dest="inputfile", 
+parser.add_option("-i", dest="inputfile",
                   action="store", type="string",
-                  help="logs/MobileContainerManager/containermanagerd.log.0 To Be Searched")
+                  help="logs/MobileInstallation/mobile_installation.log.0 To Be Searched")
 (options, args) = parser.parse_args()
 
 #no arguments given by user, print help and exit
@@ -35,8 +39,8 @@ with open(options.inputfile, 'r') as fp:
 
     for line in data:
         linecount += 1
-        
-        if '[MCMGroupManager _removeGroupContainersIfNeededforUser:groupContainerClass:identifiers:referenceCounts:]: Last reference to group container' in line:
+
+        if '[MIClientConnection uninstallIdentifiers:withOptions:completion:]' in line:
             hitcount += 1
             #print("\n" + line)
             txts = line.split()
@@ -48,9 +52,6 @@ with open(options.inputfile, 'r') as fp:
             time = txts[3]
             year = txts[4]
             group = txts[15]
-            print(day + " " + month + " " + year + " " + time + " Removed " + group + " [line " + str(linecount) + "]")
-    
+            print(day + " " + month + " " + year + " " + time + " Removed " + group + " [line " + str(linecount) + "]" + " [" + find_app(line) + "]")
+
 print("\nFound " + str(hitcount) + " group removal entries\n")
-
-
-
